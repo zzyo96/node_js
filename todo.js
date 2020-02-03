4330de6bdae9f8a68ffbd1a88dd535e10e8852e2
zzyo <1977795940@qq.com>
2020-02-03 21:25:58 +0800

    代码优化封装成函数

--- todo.js
+++ todo.js
@@ -4,17 +4,23 @@ const content = process.argv[3];
 const content2 = process.argv[4];
 const dbPath = 'C:\\Users\\admin\\Desktop\\node\\node_js\\db';
 
+//存数据库
 function save(list){
-  fs.writeFileSync(dbPath,JSON.stringify(list))  //保存数据
+  fs.writeFileSync(dbPath,JSON.stringify(list))  //序列化
+}
+
+//读数据库
+function fetch(){
+  const fileContent = fs.readFileSync(dbPath).toString()
+  const list = JSON.parse(fileContent); //反序列化
+  return list
 }
 
 switch(verb){
 	case 'add':
 		fs.stat(dbPath,function(err,stat){
 			if(err == null){
-				const fileContent = fs.readFileSync(dbPath).toString()
-				const list = JSON.parse(fileContent); //反序列化
-
+        const list = fetch();
 				const task = content
 				list.push([task,false])
         save(list);
@@ -31,29 +37,25 @@ switch(verb){
 		});
 		break;
 	case 'list':
-		const fileContent = fs.readFileSync(dbPath).toString()
-		const list = JSON.parse(fileContent); //反序列化
+    const list = fetch();
 		console.log(list)
 		break;
 	case 'delete':
-		const fileContent = fs.readFileSync(dbPath).toString()
-		const list = JSON.parse(fileContent); //反序列化
+    const list = fetch();
 		const n = content
 		list.splice(n-1,1)
 		console.log(list)
     save(list);
 		break;
 	case 'done':
-		const fileContent = fs.readFileSync(dbPath).toString()
-		const list = JSON.parse(fileContent); //反序列化
+    const list = fetch();
 		const n = content
 		list[n-1][1] = true
 		console.log(list)
     save(list);
 		break;
 	case 'edit':
-		const fileContent = fs.readFileSync(dbPath).toString()
-		const list = JSON.parse(fileContent); //反序列化
+    const list = fetch();
 		const n = content
 		list[n-1][0] = content2
 		console.log(list)
