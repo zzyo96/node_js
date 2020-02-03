dd608b1a4499beab0e32aa36338a34891b47665b
zzyo <1977795940@qq.com>
2020-02-03 20:50:45 +0800

    根据不同的动词做不同的事儿

--- todo.js
+++ todo.js
@@ -3,26 +3,32 @@ var path = require('path');
 const verb = process.argv[2]
 const content = process.argv[3]
 
-fs.stat('C:\\Users\\admin\\Desktop\\node\\node_js\\db',function(err,stat){
-	if(err == null){
-		console.log(111)
-		const fileContent = fs.readFileSync('C:\\Users\\admin\\Desktop\\node\\node_js\\db').toString()
-		const list = JSON.parse(fileContent); //反序列化
+if(verb === 'add'){
+	fs.stat('C:\\Users\\admin\\Desktop\\node\\node_js\\db',function(err,stat){
+		if(err == null){
+			const fileContent = fs.readFileSync('C:\\Users\\admin\\Desktop\\node\\node_js\\db').toString()
+			const list = JSON.parse(fileContent); //反序列化
 
-		const task = content 
-		list.push(task)
-		fs.writeFileSync('C:\\Users\\admin\\Desktop\\node\\node_js\\db',JSON.stringify(list))  //序列化
-		console.log(list)
-	}else if(err.code == 'ENOENT'){
-		fs.writeFileSync('C:\\Users\\admin\\Desktop\\node\\node_js\\db','') 
-		const list = [];
+			const task = content 
+			list.push(task)
+			fs.writeFileSync('C:\\Users\\admin\\Desktop\\node\\node_js\\db',JSON.stringify(list))  //序列化
+			console.log(list)
+		}else if(err.code == 'ENOENT'){
+			fs.writeFileSync('C:\\Users\\admin\\Desktop\\node\\node_js\\db','') 
+			const list = [];
 
-		const task = content 
-		list.push(task)
-		fs.writeFileSync('C:\\Users\\admin\\Desktop\\node\\node_js\\db',JSON.stringify(list))  //序列化
-		console.log(list)
-	}
-})
-
+			const task = content 
+			list.push(task)
+			fs.writeFileSync('C:\\Users\\admin\\Desktop\\node\\node_js\\db',JSON.stringify(list))  //序列化
+			console.log(list)
+		}
+	})
+}else if(verb === 'list'){
+	const fileContent = fs.readFileSync('C:\\Users\\admin\\Desktop\\node\\node_js\\db').toString()
+	const list = JSON.parse(fileContent); //反序列化
+	console.log(list)
+}else{
+	console.log('我不知道你要干嘛，你的动词是' + verb)
+}
 
 
